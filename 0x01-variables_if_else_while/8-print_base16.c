ec32eac2f1933726a6cd187d7336ca2036319291
root <selasiayittah3@gmail.com>
2022-03-15 06:24:07 -0700

    added a new file print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letter;
+for (letter = 48 ; letter < 58 ; letter++)
+{
+putchar(letter);
+}
+for (letter = 97 ; letter <103 ; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n');
+return (0);
+}
